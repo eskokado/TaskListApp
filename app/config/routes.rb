9c01bdad834b973a51053e879aa76d15328ae849
eskokado <eskokado@gmail.com>
2024-07-06 14:38:05 -0300

    feat/create user with devise and impl confirmable

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   namespace :backoffice do
     get 'dashboard/index'
   end
   root 'backoffice/dashboard#index'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
