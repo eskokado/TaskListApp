8251f1e4b7e53e35fc0e8caf3d504bcc504397db
eskokado <eskokado@gmail.com>
2024-07-10 06:46:58 -0300

    feat(tasks): add many attached files and validate files must be images or pdfs into model

--- app/app/models/task.rb
+++ app/app/models/task.rb
@@ -1,7 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :task_list
+  has_many_attached :files
 
   validates :name, :status, presence: true
+
+  validate :files_must_be_images_or_pdfs
 
   enum status: {
     pending: 'pending',
@@ -9,4 +12,14 @@ class Task < ApplicationRecord
   }
 
   scope :pending, -> { where(status: 'pending') }
+
+  private
+
+  def files_must_be_images_or_pdfs
+    files.each do |file|
+      unless file.content_type.in?(%w[image/jpeg image/png application/pdf])
+        errors.add(:files, 'must be an image or a PDF')
+      end
+    end
+  end
 end
